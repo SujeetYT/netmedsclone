3e3b09e4722de20c94e42dce75b9ac0f36411ec7
Vikas Yadav <vikashyadav62502@gmail.com>
2023-05-30 19:29:11 +0530

    Conneted metamask with connect button

--- src/Components/Navbar/DesktopNavbar/NavComponents/DesktopTopNavbar.jsx
+++ src/Components/Navbar/DesktopNavbar/NavComponents/DesktopTopNavbar.jsx
@@ -12,8 +12,72 @@ import { SlWallet } from "react-icons/sl";
 //   Button,
 //   Text,
 // } from "@chakra-ui/react";
+import React, { useState, useEffect } from 'react'
+
 
 const TopNavbar = () => {
+
+  const [walletAddress, setwalletAddress] = useState("");
+  useEffect(() => {
+      getCurrentWalletConnected();
+      addWalletListener();
+  });
+
+  const connectWallet = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+        try {
+            //Metamask is installed
+            const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+            setwalletAddress(accounts[0]);
+            console.log(accounts[0]);
+        } catch (err) {
+            console.log(err.message);
+        }
+    } else {
+        //Metamask is not installed
+        console.log("Please install wallet");
+    }
+
+};
+
+const getCurrentWalletConnected = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+        try {
+            const accounts = await window.ethereum.request({ method: "eth_accounts" });
+            if(accounts.length > 0)
+            {
+                setwalletAddress(accounts[0]);
+                console.log(accounts[0]);
+            }
+            else 
+            {
+                console.log("Connect to MetaMask using the connect button")
+            }
+        } catch (err) {
+            console.log(err.message);
+        }
+    } else {
+        //Metamask is not installed
+        console.log("Please install wallet");
+    }
+
+};
+
+const addWalletListener = async () => {
+    if (typeof window != "undefined" && typeof window.ethereum != "undefined") {
+        window.ethereum.on("accountsChanged", (accounts) =>
+        {
+            setwalletAddress(accounts[0]);
+            console.log(accounts[0]);
+        });
+        
+    } else {
+        //Metamask is not installed
+        setwalletAddress("");
+        console.log("Please install wallet");
+    }
+
+};
   return (
     <>
       <Box top="0px" position="fixed" width={"100%"} zIndex="1">
@@ -50,10 +114,10 @@ const TopNavbar = () => {
               _hover={{ bg: "rgb(50,174,177)" }}
               size="md"
             >
-              <Flex>
+              <Flex onClick={connectWallet} >
                 <SlWallet size={18} color="#fff" /> &nbsp;&nbsp;&nbsp;
                 <Text color={"white"} fontSize="14px" fontWeight="semibold">
-                  Connect Wallet
+                {walletAddress && walletAddress.length > 0 ? `Connected: ${walletAddress.slice(0,9)}` : "Connect Wallet"}
                 </Text>
               </Flex>
             </Button>
